4f004e3cd6e2438381fe24f959d0ab442273a727
Jacopo Antonello <jacopo@antonello.org>
2019-03-01 09:45:38 +0000

    Do not track slm/version.py

--- slm/version.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__version__ = '0.0.0+17.g2580128.dirty'
-__date__ = 'Fri Mar 1 09:19:26 2019 +0000'
-__commit__ = '25801288e2a7776e0e5400c4b845bd0b428ebb60'
