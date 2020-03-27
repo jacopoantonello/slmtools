518296bb3fd5738870b6138430ecdbb80feaf5fd
Jacopo Antonello <jacopo@antonello.org>
2020-03-27 16:42:47 +0000

    Refactor ugly slm.slm to slm.gui

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-Powershell.exe -executionpolicy bypass -NoExit -Command ". .\scripts\base.ps1; Activate-Anaconda; cd ~; python -m slm.slm"
+Powershell.exe -executionpolicy bypass -NoExit -Command ". .\scripts\base.ps1; Activate-Anaconda; cd ~; python -m slm.gui"
