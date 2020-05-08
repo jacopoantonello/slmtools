d3f1fd7d02779f17d3191bb241297065d689f4ff
Jacopo Antonello <jacopo@antonello.org>
2020-05-08 14:51:35 +0100

    Renamed project from slm to slmtools

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
 import re
-
-from subprocess import check_output
-from setuptools import setup, find_packages
+import sys
 from os import path
+from subprocess import check_output
 
+from setuptools import find_packages, setup
 
 # Get the long description from the relevant file
 here = path.abspath(path.dirname(__file__))
@@ -18,19 +17,18 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 def update_version():
     try:
-        toks = check_output(
-            'git describe --tags --long --dirty', universal_newlines=True,
-            shell=True).strip().split('-')
+        toks = check_output('git describe --tags --long --dirty',
+                            universal_newlines=True,
+                            shell=True).strip().split('-')
         version = toks[0].strip('v') + '.' + toks[1]
-        last = check_output(
-            'git log -n 1', universal_newlines=True, shell=True)
-        date = re.search(
-            r'^Date:\s+([^\s].*)$', last, re.MULTILINE).group(1)
-        commit = re.search(
-            r'^commit\s+([^\s]{40})', last, re.MULTILINE).group(1)
+        last = check_output('git log -n 1',
+                            universal_newlines=True,
+                            shell=True)
+        date = re.search(r'^Date:\s+([^\s].*)$', last, re.MULTILINE).group(1)
+        commit = re.search(r'^commit\s+([^\s]{40})', last,
+                           re.MULTILINE).group(1)
 
-        with open(
-                path.join('slm', 'version.py'), 'w', newline='\n') as f:
+        with open(path.join('slmtools', 'version.py'), 'w', newline='\n') as f:
             f.write('#!/usr/bin/env python3\n')
             f.write('# -*- coding: utf-8 -*-\n\n')
             f.write(f"__version__ = '{version}'\n")
@@ -45,13 +43,13 @@ update_version()
 
 
 def lookup_version():
-    with open(os.path.join('slm', 'version.py'), 'r') as f:
+    with open(os.path.join('slmtools', 'version.py'), 'r') as f:
         m = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
     return m.group(1)
 
 
 setup(
-    name='slm',
+    name='slmtools',
     version=lookup_version(),
     description='Spatial light modulator in Python',
     long_description=long_description,
@@ -62,10 +60,8 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering :: Physics',
-        (
-            'License :: OSI Approved :: ' +
-            'GNU General Public License v3 or later (GPLv3+)'
-        ),
+        ('License :: OSI Approved :: ' +
+         'GNU General Public License v3 or later (GPLv3+)'),
         'Programming Language :: Python :: 3',
     ],
     install_requires=['numpy', 'matplotlib', 'PyQt5', 'zernike'],
